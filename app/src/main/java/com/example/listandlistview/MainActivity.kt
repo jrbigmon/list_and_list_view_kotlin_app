c519961a8a982367ab58b70f0c4b89f007b32d0f
jrbigmon <juniorterriaga0@gmail.com>
2024-10-23 20:02:55 -0300

    learning about the lists and list view

--- app/src/main/java/com/example/listandlistview/MainActivity.kt
+++ app/src/main/java/com/example/listandlistview/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.listandlistview
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listandlistview.databinding.ActivityMainBinding
 
@@ -11,5 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val listOfNumbers = ArrayList<Int>()
+
+        listOfNumbers.add(1)
+        listOfNumbers.add(3)
+        listOfNumbers.add(7)
+        listOfNumbers.add(9)
+        listOfNumbers.add(11)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfNumbers)
+        binding.listView.adapter = adapter
+
+        binding.listView.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(
+                this,
+                "Item clicado ${listOfNumbers.get(position)} com o id ${id}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
